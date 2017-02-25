29fcacc7aea06da42216a5aedac51ecb6e806161
Jason-Pearson <Jason-Pearson@users.noreply.github.com>
2017-02-25 14:58:24 -0500

    Fixed MongoDB environment variable, testing hrefs
    In Heroku, on my app, went to Settings -> Reveal Config Vars -> in Key (type) URI, Value (type) the remote MongoDB URI - thomas:123456@ds161059.mlab.com:61059/books308
    
    Buttons aren't referenced properly, testing Add button first

--- COMP308-W2017-MidTerm-Start/server/config/db.js
+++ COMP308-W2017-MidTerm-Start/server/config/db.js
@@ -1,6 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://test:swordfish@ds145299.mlab.com:45299/testdbformidterm"
+  "URI": "mongodb://localhost/books308"
   //Remote MongoDB deployment -> let URI = "mongodb://thomas:123456@ds161059.mlab.com:61059/books308";
-//testing: "URI": "mongodb://test:swordfish@ds145299.mlab.com:45299/testdbformidterm"
 };
