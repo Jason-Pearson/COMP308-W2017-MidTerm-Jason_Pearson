2c6d33fdc16453455889815315b5dc51ec51840e
Jason-Pearson <Jason-Pearson@users.noreply.github.com>
2017-02-25 16:39:19 -0500

    made fixes

--- COMP308-W2017-MidTerm-Start/server/routes/books.js
+++ COMP308-W2017-MidTerm-Start/server/routes/books.js
@@ -55,10 +55,10 @@ router.post('/add', (req, res, next) => {
      * ADD CODE HERE *
      *****************/
 let newBook = book({
-      "title": req.body.title,
-      "price": req.body.price,
-      "author": req.body.author,
-      "genre": req.body.genre
+      "Title": req.body.title,
+      "Price": req.body.price,
+      "Author": req.body.author,
+      "Genre": req.body.genre
     });
 
     book.create(newBook, (err, book) => {
@@ -112,10 +112,10 @@ router.post('/:id', (req, res, next) => {
 
      let updatedBook = book({
        "_id": id,
-      "title": req.body.title,
-      "price": req.body.price,
-      "author": req.body.author,
-      "genre": req.body.genre
+      "Title": req.body.title,
+      "Price": req.body.price,
+      "Author": req.body.author,
+      "Genre": req.body.genre
     });
 
     book.update({_id: id}, updatedBook, (err) => {
